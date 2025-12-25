91f37f1fe4e1f8b7ef07800e6f517a5541530c42
fanjiayi <fanjiayi@cestc.cn>
2025-12-25 17:11:42 +0800

    test-1

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @author fanjiayi
+ * @date 2025/12/25 17:11
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
